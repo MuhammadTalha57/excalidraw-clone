1be4b8dc07a4fd5fd24ca8a61fbcd599f2d2ab39
Muhammad Talha <muhammadtalha.mailme@gmail.com>
2026-06-21 00:27:24 +0500

    Added create new element logic

--- web/components/canvas/interactions/captureInteraction.ts
+++ web/components/canvas/interactions/captureInteraction.ts
@@ -1,44 +1,53 @@
 import { getBoundingRectangle } from "@/utils/boundingRectangle";
 import { PointerEvent } from "react";
 import { usePreviewElementStore } from "@/stores/usePreviewElement";
+import { useSelectedToolStore } from "@/stores/useSelectedTool";
+import { useCanvasElementsStore } from "@/stores/useCanvasElements";
 
-let tool = "rectangle";
 let x = 0;
 let y = 0;
 let pointerDown = false;
 
 function onPointerDown(e: PointerEvent<HTMLCanvasElement>) {
     pointerDown = true;
-    console.log(e.clientX, e.clientY);
     x = e.clientX;
     y = e.clientY;
 }
 function onPointerUp(e: PointerEvent<HTMLCanvasElement>) {
     pointerDown = false;
-    console.log(x, y, e.clientX, e.clientY);
+    const previewElement = usePreviewElementStore.getState().previewElement;
+    const setPreviewElement =
+        usePreviewElementStore.getState().setPreviewElement;
+    if (previewElement) {
+        // Create new Canvas Element
+        const addCanvasElement =
+            useCanvasElementsStore.getState().addCanvasElement;
+        addCanvasElement(previewElement);
+        setPreviewElement(null);
+    }
 }
 
 function onPointerMove(e: PointerEvent<HTMLCanvasElement>) {
+    if (!pointerDown) return;
     let currentX = e.clientX;
     let currentY = e.clientY;
+    let tool = useSelectedToolStore.getState().selectedTool;
     const setPreviewElement =
         usePreviewElementStore.getState().setPreviewElement;
-    if (pointerDown) {
-        const boundingRect = getBoundingRectangle(x, y, currentX, currentY);
-        const previewElement: CanvasElement = {
-            id: "2",
-            type: "rectangle",
-            strokeColor: "#000000",
-            fillColor: "#000000",
-            x: boundingRect.x,
-            y: boundingRect.y,
-            width: boundingRect.width,
-            height: boundingRect.height,
-        };
-        if (tool === "rectangle") {
-            // Drawing Rectangle
-            setPreviewElement(previewElement);
-        }
+    const boundingRect = getBoundingRectangle(x, y, currentX, currentY);
+    const previewElement: CanvasElement = {
+        id: "2",
+        type: "rectangle",
+        strokeColor: "#000000",
+        fillColor: "#000000",
+        x: boundingRect.x,
+        y: boundingRect.y,
+        width: boundingRect.width,
+        height: boundingRect.height,
+    };
+    if (tool === "rectangle") {
+        // Drawing Rectangle
+        setPreviewElement(previewElement);
     }
 }
 
