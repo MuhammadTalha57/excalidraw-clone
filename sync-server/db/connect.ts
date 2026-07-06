0fe58de589da387c44bfa7170b6283e5c0932b06
Muhammad Talha <muhammadtalha.mailme@gmail.com>
2026-07-06 18:18:32 +0500

    Updated directory structure

---
+++ sync-server/db/connect.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+declare global {
+  var _mongooseConn: Promise<typeof mongoose> | undefined;
+}
+
+export function connectDB() {
+  if (!global._mongooseConn) {
+    global._mongooseConn = mongoose.connect(process.env.MONGODB_URI!);
+  }
+  return global._mongooseConn;
+}
---
+++ sync-server/utils/id.ts
@@ -0,0 +1,9 @@
+import { customAlphabet, nanoid } from "nanoid";
+
+
+const ALPHABET = "23456789abcdefghjkmnpqrstuvwxyz";
+
+export const generateSessionId = customAlphabet(ALPHABET, 8);
+
+
+export const generateHostToken = () => nanoid(24);
